423136abbf185e132dcf74c88446b7b2bb9938da
Diljot Singh <singhdiljot2306@gmail.com>
2022-06-06 18:57:56 +0530

    done with app

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 console.log("Successfully fetched image");
 
-async function loadNames() {
+let copyVar;
+
+async function loadImage() {
     const response = await fetch('https://dog.ceo/api/breeds/image/random');
     const images = await response.json();
-    console.log(images.message); 
-  }
 
-loadNames();
+    var putImg = images.message;
+
+    copyVar = putImg;
+
+    document.getElementById("dynamicImg").src = putImg;
+    
+    setTimeout(() => {
+        document.getElementById("btbn").innerText = "Next Image";
+        document.getElementById("url").innerText = "Get URL";
+        btbn.removeAttribute('disabled', '');
+    }, 1000);
+}
+
+function copyUrl() {
+    // console.log(copyVar);
+    navigator.clipboard.writeText(copyVar);
+    document.getElementById("url").innerText = "Copied";
+}
+
+function nextImg() {
+    loadImage();
+
+    if(document.getElementById("btbn").innerText != "Waiting.."){
+        document.getElementById("btbn").innerText = "Waiting..";
+        btbn.setAttribute('disabled', '');
+    }
+    
+}
+
+loadImage();
